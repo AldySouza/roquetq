1e6fb44a42709af567818c1a7f64df6d802c1cab
Aldy Souza <60198926+AldySouza@users.noreply.github.com>
2022-02-11 12:21:56 -0300

    discovery trail

---
+++ src/server.js
@@ -0,0 +1,19 @@
+
+
+const express = require('express');
+const route = require('./routes');
+const path = require('path')
+
+const server = express();
+
+server.set('view engine', 'ejs')
+
+server.use(express.static("public"))
+
+server.set('views', path.join(__dirname, 'views'))
+
+server.use(express.urlencoded({extended: true}))
+
+server.use(route);
+
+server.listen(3030, () => console.log('Server is running!'));
